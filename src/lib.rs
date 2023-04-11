617897e8df496f470ad1773d4c3974a8a51ba32c
EvilMadSquirrel <minichev.s.l@gmail.com>
2023-04-11 10:52:04 +0300

    implemented library

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,81 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use byteorder::{LittleEndian, ReadBytesExt};
+use crc::crc32;
+use serde_derive::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    fs::{File, OpenOptions},
+    io::{self, BufReader, Read, Seek, SeekFrom},
+    path::Path,
+};
+
+type ByteString = Vec<u8>;
+
+type ByteStr = [u8];
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyValuePair {
+    pub key: ByteString,
+    pub value: ByteString,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+pub struct AktionKV {
+    f: File,
+    pub index: HashMap<ByteString, u64>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl AktionKV {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        let f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(path)?;
+        let index = HashMap::new();
+        Ok(AktionKV { f, index })
+    }
+
+    pub fn load(&mut self) -> io::Result<()> {
+        let mut f = BufReader::new(&mut self.f);
+        loop {
+            let position = f.seek(SeekFrom::Current(0))?;
+            let maybe_kv = AktionKV::process_record(&mut f);
+
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => match err.kind() {
+                    io::ErrorKind::UnexpectedEof => {
+                        break;
+                    }
+                    _ => return Err(err),
+                },
+            };
+            self.index.insert(kv.key, position);
+        }
+        Ok(())
+    }
+    fn process_record<R: Read>(f: &mut R) -> io::Result<KeyValuePair> {
+        let saved_checksum = f.read_u32::<LittleEndian>()?;
+        let key_len = f.read_u32::<LittleEndian>()?;
+        let val_len = f.read_u32::<LittleEndian>()?;
+        let data_len = key_len + val_len;
+
+        let mut data = ByteString::with_capacity(data_len as usize);
+
+        {
+            f.by_ref().take(data_len as u64).read_to_end(&mut data)?;
+        }
+        debug_assert_eq!(data.len(), data_len as usize);
+        let checksum = crc32::checksum_ieee(&data);
+        if checksum != saved_checksum {
+            panic!(
+                "data corruption encountered ({:08x} != {:08x})",
+                checksum, saved_checksum
+            );
+        }
+        let value = data.split_off(key_len as usize);
+        let key = data;
+        Ok(KeyValuePair { key, value })
     }
 }
